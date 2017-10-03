8d1d106b249338e6abc410b77229a14b7da3f669
TerenikDmitry <dm.terenik@gmail.com>
2017-10-03 20:37:15 +0300

    CRUD

--- webserver.py
+++ webserver.py
@@ -2,11 +2,18 @@ from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import cgi
 
 import os
+import ntpath
 
+## import CRUD operation
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
 from database_setup import Restaurant, Base, MenuItem
+
+## Create session and connect to DB
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 # Styles and scripting for the page
 main_page = '''
@@ -36,12 +43,14 @@ class WebServerHandler(BaseHTTPRequestHandler):
 	def do_GET(self):
 		try:
 			if self.path.endswith(".css"):
-				f = open(os.curdir+os.sep+self.path)
+				head, tail = os.path.split(self.path)
+				f = open(os.curdir+os.sep+"css/"+tail)
 				self.send_response(200)
 				self.send_header('Content-type', 'text/css')
 				self.end_headers()
 				self.wfile.write(f.read())
 				f.close()
+				self.wfile.write(os.curdir+os.sep+"css/"+tail)
 				return
 			if self.path.endswith("/hello"):
 				self.send_response(200)
@@ -54,7 +63,8 @@ class WebServerHandler(BaseHTTPRequestHandler):
 						<h2>Hello</h2>
 						<form method="POST" action="hello" enctype="multipart/form-data">
 							<h2> What would you like me to say? </h2>
-							<input type="text" name="message"><input type="submit" value="Submit">
+							<input type="text" name="message">
+							<input type="submit" value="Submit">
 						</form>
 					</div>
 				</div>
@@ -72,23 +82,65 @@ class WebServerHandler(BaseHTTPRequestHandler):
 					<div class="container">
 						<h2>Add a new Restaurant: </h2>
 						<form method="POST" action="new" enctype="multipart/form-data">
-							<input type="text" name="nameOfRestaurant" placeholder="Name of new restaurant"><input type="submit" value="Submit">
+							<input type="text" name="nameOfRestaurant" placeholder="Name of new restaurant">
+							<input type="submit" value="Add">
 						</form>
+						<a class="button button-primary" href="/restaurants">Back to the list of restaurants</a>
 					</div>
 				</div>
 				'''
 
 				self.wfile.write(main_page.format(content=output))
 				return
+			if self.path.endswith("/edit"):
+				idOfRestaurant = self.path.split("/")[-2]
+				restaurant = session.query(Restaurant).filter_by(id = idOfRestaurant).one()
+				if restaurant != []:
+					self.send_response(200)
+					self.send_header('Content-type', 'text/html')
+					self.end_headers()
+
+					output = '''
+					<div class="section values">
+						<div class="container">
+							<h2>Edit "{old_name}"</h2>
+							<form method="POST" action="edit" enctype="multipart/form-data">
+								<input type="text" name="nameOfRestaurant" placeholder="New name">
+								<input type="submit" value="Edit">
+							</form>
+							<a class="button button-primary" href="/restaurants">Back to the list of restaurants</a>
+						</div>
+					</div>
+					'''
+
+				self.wfile.write(main_page.format(content=output.format(old_name=restaurant.name)))
+			if self.path.endswith("/delete"):
+				idOfRestaurant = self.path.split("/")[-2]
+				restaurant = session.query(Restaurant).filter_by(id = idOfRestaurant).one()
+				if restaurant != []:
+					self.send_response(200)
+					self.send_header('Content-type', 'text/html')
+					self.end_headers()
+
+					output = '''
+					<div class="section values">
+						<div class="container">
+							<h2>Delete "{old_name}"?</h2>
+							<form method="POST" action="delete" enctype="multipart/form-data">
+								<a class="button button-primary" href="/restaurants">Back</a>
+								<input class="button-red" type="submit" value="Delete">
+							</form>
+						</div>
+					</div>
+					'''
+
+				self.wfile.write(main_page.format(content=output.format(old_name=restaurant.name)))
 			if self.path.endswith("/restaurants"):
+				# Get List of restaurants
+				restaurants = session.query(Restaurant).all()
 				self.send_response(200)
 				self.send_header('Content-type', 'text/html')
 				self.end_headers()
-
-				engine = create_engine('sqlite:///restaurantmenu.db')
-				Base.metadata.bind = engine
-				DBSession = sessionmaker(bind=engine)
-				sessinon = DBSession()	
 
 				# Restaurants entry html template
 				list_restaurant_html = '''
@@ -100,29 +152,25 @@ class WebServerHandler(BaseHTTPRequestHandler):
 				list_restaurant_element_html = '''
 				<div class="row">
 					<h3>{name}</h3>
-					<a class="button button-primary" href="#">Edit</a>
-					<a class="button button-red" href="#">Delete</a>
+					<a class="button button-primary" href="/restaurants/{id}/edit">Edit</a>
+					<a class="button button-red" href="/restaurants/{id}/delete">Delete</a>
 				</div>
 				'''
 				# New Restaurant html template
 				add_block_html = '''
 				<div class="container values">
 					<div class="row">
-						<a class="button button-primary" href="/new">Add a new Restaurant</a>
+						<a class="button button-primary" href="/restaurants/new">Add a new Restaurant</a>
 					</div>
 				</div>
 				'''
 
-				restaurants = sessinon.query(Restaurant).all()
 				listRestaurantElements = ""
-				# Get List of restaurants
 				for restaurant in restaurants:
-					listRestaurantElements += list_restaurant_element_html.format(name=restaurant.name)
+					listRestaurantElements += list_restaurant_element_html.format(name=restaurant.name,id=restaurant.id)
 				listRestaurant = list_restaurant_html.format(content=listRestaurantElements)
 
-				output = listRestaurant + add_block_html
-
-				self.wfile.write(main_page.format(content=output))
+				self.wfile.write(main_page.format(content=(add_block_html + listRestaurant)))
 				return
 		except IOError:
 			self.send_error(404, 'File Not Found: %s' % self.path)
@@ -133,15 +181,17 @@ class WebServerHandler(BaseHTTPRequestHandler):
 			self.end_headers()
 
 			ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
-			if ctype == 'multipart/form-data':
-				fields = cgi.parse_multipart(self.rfile, pdict)
-				messagecontent = fields.get('message')
+			output = ""
 
-			output = '''
+			if self.path.endswith("/hello"):
+				if ctype == 'multipart/form-data':
+					fields = cgi.parse_multipart(self.rfile, pdict)
+					messagecontent = fields.get('message')
+
+				helloAndText = '''
 				<div class="section values">
 					<div class="container">
-						<h2>Okay, how about this:</h2>
-						<h2>{text}</h2>
+						<h2>You say: {text}</h2>
 						<form method="POST" action="hello" enctype="multipart/form-data">
 							<h2> What would you like me to say? </h2>
 							<input type="text" name="message"><input type="submit" value="Submit">
@@ -149,8 +199,77 @@ class WebServerHandler(BaseHTTPRequestHandler):
 					</div>
 				</div>
 				'''
+				output = helloAndText.format(text=messagecontent[0])
+			if self.path.endswith("/restaurants/new"):
+				if ctype == 'multipart/form-data':
+					fields = cgi.parse_multipart(self.rfile, pdict)
 
-			self.wfile.write(main_page.format(content=output.format(text=messagecontent[0])))
+				messagecontent = fields.get("nameOfRestaurant")[0]
+				restaurant = Restaurant(name = messagecontent)
+				session.add(restaurant)
+				session.commit()
+
+				result = '''
+				<div class="section values">
+					<div class="container">
+						<h2>Restaurant '{content}' added!</h2>
+						<a class="button button-primary" href="/restaurants">Back to the list of restaurants</a>
+					</div>
+				</div>
+				'''
+
+				output = result.format(content=messagecontent)
+			if self.path.endswith("/delete"):
+				idOfRestaurant = self.path.split("/")[-2]
+
+				restaurant = session.query(Restaurant).filter_by(id = idOfRestaurant).one()
+				if restaurant != []:
+					result = '''
+					<div class="section values">
+						<div class="container">
+							<h2>Deleted '{old}'!</h2>
+							<a class="button button-primary" href="/restaurants">Back to the list of restaurants</a>
+						</div>
+					</div>
+					'''
+					output = result.format(old=restaurant.name)
+
+					session.delete(restaurant)
+					session.commit()
+				else:
+					output = '''
+					<div class="section values">
+						<div class="container">
+							<h2>ERROR</h2>
+							<a class="button button-primary" href="/restaurants">Back to the list of restaurants</a>
+						</div>
+					</div>
+					'''
+			if self.path.endswith("/edit"):
+				if ctype == 'multipart/form-data':
+					fields = cgi.parse_multipart(self.rfile, pdict)
+
+				newName = fields.get("nameOfRestaurant")[0]
+				idOfRestaurant = self.path.split("/")[-2]
+
+				restaurant = session.query(Restaurant).filter_by(id = idOfRestaurant).one()
+				if restaurant != []:
+					result = '''
+					<div class="section values">
+						<div class="container">
+							<h2>Old name '{old}' added!</h2>
+							<h2>New name '{new}' added!</h2>
+							<a class="button button-primary" href="/restaurants">Back to the list of restaurants</a>
+						</div>
+					</div>
+					'''
+					output = result.format(old=restaurant.name,new=newName)
+
+					restaurant.name = newName
+					session.add(restaurant)
+					session.commit()
+
+			self.wfile.write(main_page.format(content=output))
 			return
 		except:
 			pass
